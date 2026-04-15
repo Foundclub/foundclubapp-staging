ccdd54ce1e9c171596e62a09cdcafac9dc73a47f
Foundclub <foundclubpro@gmail.com>
2026-04-15 23:27:59 +0200

    fix: disable unused keyboard controller on android

--- react-native.config.js
+++ react-native.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   assets: ['./assets/fonts/'],
+  dependencies: {
+    'react-native-keyboard-controller': {
+      platforms: {
+        android: null,
+      },
+    },
+  },
   project: {
     android: {},
     ios: {},
